ed5c37e472d517cbcbf334963d95b7cc4cc5d200
glitch <mdpolokahmed825@gmail.com>
2026-08-23 21:04:43 +0600

    added area chart in dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,19 +1,26 @@
+import GraphData from "@/components/dashboard/overview/GraphData";
 import UserProfile from "@/components/dashboard/overview/UserProfile";
 import UserStats from "@/components/dashboard/overview/UserStats";
 import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 const Dashboard = () => {
+
   return (
     <div>
       <div className="my-4">
         <h1 className="text-2xl font-semibold">Dashboard Overview</h1>
         <p className="text-primary">Manage your projects and endpoints.</p>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-4 my-4">
         <UserStats></UserStats>
         <Separator orientation="vertical"></Separator>
         <UserProfile></UserProfile>
+      </div>
+      <Separator></Separator>
+
+      <div className="my-4">
+        <GraphData></GraphData>
       </div>
     </div>
   );
